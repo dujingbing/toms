a73b01b57566a49338342a065d4f657ebcd804d1
dujingbing <dujingbing_csu@163.com>
2019-06-09 17:50:25 +0800

    add codes of 'toms-front'.

--- toms-front/include/server.h
+++ toms-front/include/server.h
@@ -1,4 +1,5 @@
 #include <include/http.h>
+#include <deps/uv/include/uv.h>
 
 namespace server
 {
@@ -7,13 +8,22 @@ class ProtoServer;
 
 class HttpServer
 {
+private:
+    static const int DEFAULT_PORT = 6666;
+
+    static const int MAX_CONNECT_SIZE = 10000;
+
+    static void OnConnected(uv_stream_t* server, int status);
+
+    uv_loop_t *loop;
+
 public:
     HttpServer();
     HttpServer(int port);
     ~HttpServer();
 
-    bool listen();
-    bool listen(int port);
+    bool Listen();
+    bool Listen(int port);
 };
 
 class ProtoServer
@@ -23,8 +33,8 @@ public:
     ProtoServer(int port);
     ~ProtoServer();
 
-    bool listen();
-    bool listen(int port);
+    bool Listen();
+    bool Listen(int port);
 };
 
 } // namespace server
--- toms-front/src/server.cc
+++ toms-front/src/server.cc
@@ -14,12 +14,54 @@ HttpServer::~HttpServer()
 {
 }
 
-bool HttpServer::listen()
+void HttpServer::OnConnected(uv_stream_t *server, int status)
 {
 }
 
-bool HttpServer::listen(int port)
+bool HttpServer::Listen()
 {
+    Listen(DEFAULT_PORT);
+}
+
+bool HttpServer::Listen(int port)
+{
+    if (port < 1024 || port > 49150)
+    {
+        fprintf(stderr, "Toms-front server initialized error, server port(%d) is invalid.\n", port);
+        exit(1);
+    }
+
+    loop = uv_default_loop();
+
+    uv_tcp_t server;
+    int tcp_init = uv_tcp_init(loop, &server);
+    if (tcp_init != 0)
+    {
+        fprintf(stderr, "%s: %s.\n", "Toms-front server initialized error, detail", uv_err_name(tcp_init));
+        exit(1);
+    }
+
+    sockaddr_in address;
+    int ip4_addr = uv_ip4_addr("127.0.0.1", port, &address);
+    if (ip4_addr != 0)
+    {
+        fprintf(stderr, "%s: %s.\n", "Toms-front server initialized error, detail", uv_err_name(ip4_addr));
+        exit(1);
+    }
+
+    int tcp_bind = uv_tcp_bind(&server, (const struct sockaddr *)&address, 0);
+    if (tcp_bind != 0)
+    {
+        fprintf(stderr, "%s: %s.\n", "Toms-front server initialized error, detail", uv_err_name(tcp_bind));
+        exit(1);
+    }
+
+    int listen = uv_listen((uv_stream_t *)&server, MAX_CONNECT_SIZE, OnConnected);
+    if (listen != 0)
+    {
+        fprintf(stderr, "%s: %s.\n", "Toms-front server initialized error, detail", uv_err_name(listen));
+        exit(1);
+    }
 }
 
 ProtoServer::ProtoServer()
@@ -34,10 +76,10 @@ ProtoServer::~ProtoServer()
 {
 }
 
-bool ProtoServer::listen()
+bool ProtoServer::Listen()
 {
 }
 
-bool ProtoServer::listen(int port)
+bool ProtoServer::Listen(int port)
 {
 }
