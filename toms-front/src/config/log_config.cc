87e7b605bc013f1c80d5fce9d10f51d07d0c390f
dujingbing <dujingbing_csu@163.com>
2019-06-25 22:50:00 +0800

    add codes of 'toms-front'.

---
+++ toms-front/src/config/log_config.cc
@@ -0,0 +1,27 @@
+#include <include/common.h>
+
+using namespace config;
+
+LogConfig::LogConfig()
+{
+}
+
+LogConfig::~LogConfig()
+{
+}
+
+void LogConfig::SetHome(const char *home)
+{
+}
+
+const char *LogConfig::GetHome()
+{
+}
+
+void LogConfig::SetMaxFileSize(int size)
+{
+}
+
+int LogConfig::GetMaxFileSize()
+{
+}
